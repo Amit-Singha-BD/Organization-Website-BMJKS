8b4a2de0f02560d8a6a05489ccc4f79a566d58f1
ShyamolSingha <dev.shyamolsingha@gmail.com>
2025-10-11 17:12:20 +0600

    home news dynamic

--- app/Http/Controllers/frontend/FrontendHomeController.php
+++ app/Http/Controllers/frontend/FrontendHomeController.php
@@ -2,13 +2,16 @@
 namespace App\Http\Controllers\frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use App\Models\Service;
 
 class FrontendHomeController extends Controller{
 
     public function home_view(){
+        $response = Http::get('https://mcqbankbd.com/blog/wp-json/wp/v2/posts?per_page=4&_fields=id,date,title,excerpt,link');
+        $posts = $response->json();
         $services = Service::all();
-        return view('frontend.pages.home', compact('services'));
+        return view('frontend.pages.home', compact('services', 'posts'));
     }
 
 }
