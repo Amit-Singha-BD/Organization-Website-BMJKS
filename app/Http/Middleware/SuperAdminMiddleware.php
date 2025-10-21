518f01faea4fb146ea8ce62f77468aa0e976e3f0
Amit-Singha-BD <dev.amitsingah@gmail.com>
2025-10-21 22:31:24 +0600

    middleware create

---
+++ app/Http/Middleware/SuperAdminMiddleware.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class SuperAdminMiddleware {
+
+    public function handle(Request $request, Closure $next): Response {
+        if(Auth::check() && Auth::user()->account_type === 'superadmin'){
+            return $next($request);
+        }
+        else{
+            return redirect()->back();
+        }
+    }
+}
