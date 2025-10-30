38f4e1beb2963d931ae8065f22b7db8a9affd75a
dev-roni <dev.ronisingha@gmail.com>
2025-10-30 22:32:00 +0600

    notice ,donation soho aro onek kaj ses hoyeche

--- app/Http/Controllers/frontend/DonetorController.php
+++ app/Http/Controllers/frontend/DonetorController.php
@@ -12,14 +12,16 @@ class DonetorController extends Controller
 {
     public function donetorFrontend()
     {
-        $persons = Person::where('donator', 'yes')
-            ->with('donations.event')
-            ->withSum('donations', 'donate_amount')
-            ->orderByDesc(Donation::select('date')->whereColumn('people_id', 'people.id')->latest('date')->limit(1))
+        $donations = Donation::with(['person', 'event'])
+            ->whereHas('person', function ($query) {
+                $query->where('donator', 'yes');
+            })
+            ->orderByDesc('date') // সর্বশেষ ডোনেশন আগে দেখাবে
             ->paginate(10);
 
-    return view('frontend.pages.donetor_list', compact('persons'));
+        return view('frontend.pages.recent_donation', compact('donations'));
     }
+
     public function topDonetorFrontend()
     {
         $persons = Person::where('donator', 'yes')
