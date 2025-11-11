90093853dbaa82452995bbc82f268367c3e9a630
dev-roni <dev.ronisingha@gmail.com>
2025-11-11 21:40:59 +0600

    chada module add

--- app/Http/Controllers/backend/DashboardController.php
+++ app/Http/Controllers/backend/DashboardController.php
@@ -10,8 +10,66 @@ use App\Models\PersonTag;
 use App\Models\CommitteeYear;
 use App\Models\CommitteeMember;
 use App\Models\ViewCount;
+use App\Models\ChadaName;
+use App\Models\ChadaCollection;
 
 class DashboardController extends Controller {
+
+    public function __construct()
+    {
+        $month = date('m');
+        $year = date('Y');
+
+        $english = ['0','1','2','3','4','5','6','7','8','9'];
+        $bangla  = ['০','১','২','৩','৪','৫','৬','৭','৮','৯'];
+        $year_bn = str_replace($english, $bangla, $year);
+
+        $banglaMonths = [
+            '01' => 'জানুয়ারি',
+            '02' => 'ফেব্রুয়ারি',
+            '03' => 'মার্চ',
+            '04' => 'এপ্রিল',
+            '05' => 'মে',
+            '06' => 'জুন',
+            '07' => 'জুলাই',
+            '08' => 'আগস্ট',
+            '09' => 'সেপ্টেম্বর',
+            '10' => 'অক্টোবর',
+            '11' => 'নভেম্বর',
+            '12' => 'ডিসেম্বর',
+        ];
+
+        $month_name = $banglaMonths[$month] . ' ' . $year_bn;
+
+        $chada_name_data = ChadaName::whereYear('date', $year)
+            ->whereMonth('date', $month)
+            ->first();
+
+        if (!$chada_name_data) {
+
+            \DB::transaction(function () use ($month_name, $year, $month) {
+                $chada_data = ChadaName::create([
+                    'chada_name' => $month_name,
+                    'date' => date('Y-m-d'),
+                ]);
+
+
+                ChadaCollection::create([
+                    'chada_names_id' => $chada_data->id,
+                    'committee_id' => 1,
+                    'amount' => 2100,
+                ]);
+
+                for ($committee_id = 2; $committee_id <= 13; $committee_id++) {
+                    ChadaCollection::create([
+                        'chada_names_id' => $chada_data->id,
+                        'committee_id' => $committee_id,
+                        'amount' => 250,
+                    ]);
+                }
+            });
+        }
+    }
 
     public function Dashboard(){
         $notices = Notice::orderBy('id', 'desc')->take(5)->get();
