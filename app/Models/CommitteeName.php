d44aa2d2c1a21bb53f052f03790059885a0101c4
dev-roni <dev.ronisingha@gmail.com>
2025-11-12 19:29:02 +0600

    chadar relation ses

--- app/Models/CommitteeName.php
+++ app/Models/CommitteeName.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\ChadaCollection;
 
 class CommitteeName extends Model {
 
@@ -13,4 +14,8 @@ class CommitteeName extends Model {
         'committee_name',
         'committee_slug',
     ];
+    public function chada() {
+        return $this->hasMany(ChadaCollection::class,'committee_id','id');
+    }
+
 }
